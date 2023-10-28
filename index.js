c23d8b02b2864e484adf7311508d75c030d915dd
twashi1 <thomas.zarri@durham.ac.uk>
2023-10-28 08:00:39 +0100

    Removed CSS navbar in favour of Bootstrap navbar. Fixes #12 and #14

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-function hamburgerToggle(event) {
-    let navbarLinks = document.getElementById("navbarLinksDiv");
-
-    navbarLinks.style.display = navbarLinks.style.display === "block" ? "none" : "block";
-}
-
 document.addEventListener("DOMContentLoaded", () => {
-    let hamburgerIcon = document.getElementById("hamburgerToggleLink");
-
-    hamburgerIcon.addEventListener("click", hamburgerToggle);
+    
 });
